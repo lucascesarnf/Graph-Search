fc858775c6da5939ab725de16825206cf6c115b1
Lucas César <lucascesarnf@hotmail.com>
2019-03-20 13:47:23 -0300

    Initial Commit

---
+++ Graph/GameViewController.swift
@@ -0,0 +1,50 @@
+//
+//  GameViewController.swift
+//  Graph
+//
+//  Created by Lucas César  Nogueira Fonseca on 20/03/19.
+//  Copyright © 2019 Lucas César  Nogueira Fonseca. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let view = self.view as! SKView? {
+            // Load the SKScene from 'GameScene.sks'
+            if let scene = SKScene(fileNamed: "GameScene") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            }
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+
+    override var shouldAutorotate: Bool {
+        return true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
